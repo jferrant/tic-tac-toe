800e49490ea2d5fa5c969e94b634d396514569a4
Jacinta Ferrant <jacinta.ferrant@gmail.com>
2026-04-19 16:44:46 -0700

    feat: add server main tcp listener
    Signed-off-by: Jacinta Ferrant <jacinta.ferrant@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,37 @@
 //! This crate will impelement the stateful coordinator and "Prover" for the
 //! zkVM-provable Tic-Tac-Toe game. It will bridge the gap between signed user
 //! actions and the pure State Transition Function (STF).
-fn main() {
-    unimplemented!("Implement ZK-Server");
+
+use std::sync::Arc;
+use tracing::info;
+
+use axum::{routing::post, Router};
+
+use crate::{
+    handlers::{handle_create, handle_play},
+    state::AppState,
+};
+pub mod handlers;
+pub mod models;
+pub mod state;
+
+#[tokio::main]
+async fn main() {
+    // Init logging
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let shared_state = Arc::new(AppState::default());
+
+    let app = Router::new()
+        .route("/game/create", post(handle_create))
+        .route("/game/play", post(handle_play))
+        .with_state(shared_state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    info!("Server listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
 }
